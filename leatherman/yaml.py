041affeb497eb01dad9df045636c1acea403c031
Scott A. Idler <scott.a.idler@gmail.com>
2021-12-10 14:44:02 -0800

    added OrderedDict to yaml

--- leatherman/yaml.py
+++ leatherman/yaml.py
@@ -1,31 +1,37 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""
+'''
 output
-"""
+'''
 from ruamel import yaml
+from collections import OrderedDict
 
 
 def str_presenter(dumper, data):
-    str_tag = "tag:yaml.org,2002:str"
+    str_tag = 'tag:yaml.org,2002:str'
     if len(data.splitlines()) > 1:
-        return dumper.represent_scalar(str_tag, data, style="|")
+        return dumper.represent_scalar(str_tag, data, style='|')
     return dumper.represent_scalar(str_tag, data)
-
 
 yaml.add_representer(str, str_presenter)
 
 
 def list_presenter(dumper, data):
-    list_tag = "tag:yaml.org,2002:seq"
+    list_tag = 'tag:yaml.org,2002:seq'
     if len(data) > 1:
         if all([isinstance(item, str) for item in data]):
             return dumper.represent_sequence(list_tag, data, flow_style=False)
     return dumper.represent_sequence(list_tag, data)
 
-
 yaml.add_representer(list, list_presenter)
+
+
+def OrderedDict_presenter(dumper, data):
+    map_tag = 'tag:yaml.org,2002:map'
+    return dumper.represent_mapping(map_tag, data.items())
+
+yaml.add_representer(OrderedDict, OrderedDict_presenter)
 
 
 def yaml_format(obj):
