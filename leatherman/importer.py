d2dcd5ccfbe76222c3dc516a8303cfda9aab42cb
Scott A. Idler <scott.a.idler@gmail.com>
2025-02-10 06:56:10 -0800

    upgraded to importlib.util

--- leatherman/importer.py
+++ leatherman/importer.py
@@ -2,9 +2,18 @@
 # -*- coding: utf-8 -*-
 
 import os
-import imp
+import importlib.util
 
 
 def import_modules(dirpath, endswith):
     files = [f for f in os.listdir(dirpath) if f.endswith(endswith)]
-    return [imp.load_source(f.split(".")[0], dirpath + "/" + f) for f in files]
+    modules = []
+    for f in files:
+        module_name = f.split(".")[0]
+        file_path = os.path.join(dirpath, f)
+        spec = importlib.util.spec_from_file_location(module_name, file_path)
+        module = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        modules.append(module)
+    return modules
+
